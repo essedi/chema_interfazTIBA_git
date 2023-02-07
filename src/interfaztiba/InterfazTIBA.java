0103e6cf1fb0873ecd5f32be029dbee8a406b3d9
Jesús Olmeda <jesus.olmeda@exos-solutions.com>
2023-02-07 12:36:55 +0100

    0006945: Falla la integración con TIBA

--- src/interfaztiba/InterfazTIBA.java
+++ src/interfaztiba/InterfazTIBA.java
@@ -5,9 +5,7 @@
  */
 package interfaztiba;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -16,16 +14,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.Reader;
-import java.io.StringReader;
-import java.lang.reflect.Field;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -50,11 +39,6 @@ import jcifs.smb.NtlmPasswordAuthentication;
 import jcifs.smb.SmbFile;
 import jcifs.smb.SmbFileInputStream;
 
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.Namespace;
-import org.jdom2.input.SAXBuilder;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
@@ -79,13 +63,14 @@ public class InterfazTIBA {
     private static final String POSTGRESQL_URL = "jdbc:postgresql://" + postgresqlIp + ":" + postgresqlPort + "/"
             + postgresqlDatabaseName + "?user=" + postgresqlUser + "&password=" + postgresqlPassword;
     
-    private static String imagePath = "/home/exos/planificador/cbdriver/images/estados/";
 
     private static final File textFile = new File("./tmp/Datos_ChemaBallester_" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmm")) + ".txt");
-//    private static final File textFile = new File("\\" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmm")) + ".txt");
     
     private static final Map<String, List<String>> ultimosEventos = new HashMap<>();
     private static final Map<String, List<String>> imagenesNameFile = new HashMap<>();
+    private static List<String> vehiculosEnviados = new ArrayList<>();
+
+    
 
     public static void main(String[] args) {
         inicio();
@@ -93,6 +78,7 @@ public class InterfazTIBA {
         createTextFile();
         anyadirEstadosCBDriver();
         anyadirEstadosPosicion();
+        anyadirVehiculosConRutasNoFinalizadas();
         sendTextFileToTiba();
         sendImagesToTiba();
         updateEstadosEnviados();
@@ -138,7 +124,6 @@ public class InterfazTIBA {
             ftpPort = properties.getProperty("ftpPort", "21");
             ftpUser = properties.getProperty("ftpUser", "cballester");
             ftpPassword = properties.getProperty("ftpPssword", "NabwR2Wzy7nXe9n");
-            imagePath = String.valueOf(properties.getProperty("imagePath", ""));
         } catch (FileNotFoundException ex) {
             LOGGER.log(Level.SEVERE, "", ex);
         } catch (IOException ex) {
@@ -173,7 +158,8 @@ public class InterfazTIBA {
                     + "WHERE cli.codigo = '2200' AND tipo_aux IS NOT NULL  AND tipo_aux != '' AND enviado_tiba = false "
                     + "and a.definitiva = true "
                     + "AND (SELECT (MAX(e2.tipo_aux) IS NULL OR MAX(e2.tipo_aux) < '41') "
-                    + "FROM m_estado_ruta e2 WHERE e2.g_hoja_ruta_id = e.g_hoja_ruta_id AND e2.enviado_tiba = true);";
+                    + "FROM m_estado_ruta e2 WHERE e2.g_hoja_ruta_id = e.g_hoja_ruta_id AND e2.enviado_tiba = true) "
+                    + "AND  e.tipo_aux =  (SELECT MAX(e3.tipo_aux) FROM m_estado_ruta e3 WHERE e3.g_hoja_ruta_id = e.g_hoja_ruta_id);";
             try (Statement st = postgresqlConnection.createStatement(); ResultSet rs = st.executeQuery(selectSql)) {
                 ResultSetMetaData rsmd = rs.getMetaData();
                 int contador = 0;
@@ -234,9 +220,12 @@ public class InterfazTIBA {
 
     private static void anyadirEstadosPosicion() {
         ultimosEventos.forEach((t, u) -> {
+            //u.get(4) -> tipo_aux
             if (!u.get(4).equals("41")) {
                 // Cambiar método de obtención de posición
                 //List<String> posicion = getPosicionMovilData(u.get(5));
+                //u.get(5) -> matricula
+                
                 List<String> posicion = getPosicionBBDD(u.get(5));
                 String fecha = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
                 String hora = LocalTime.now().format(DateTimeFormatter.ofPattern("HHmm"));
@@ -249,100 +238,12 @@ public class InterfazTIBA {
                             + posicion.get(0) + ";" + posicion.get(1) + ";" + fecha + ";" + hora + ";\"\"\n");
 
                     writer.close();
+                    vehiculosEnviados.add(u.get(5));
                 } catch (IOException ex) {
                     Logger.getLogger(InterfazTIBA.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         });
-    }
-
-    private static List<String> getPosicionMovilData(String matriculaCabeza) {
-        List<String> posicion = new ArrayList<>();
-        try {
-            //Code to make a webservice HTTP request
-            String responseString = "";
-            String outputString = "";
-            String wsURL = "https://ws.movildata.com/wsUsers.asmx?";
-            URL url_asaas = new URL(wsURL);
-            URLConnection connection = url_asaas.openConnection();
-            HttpURLConnection httpConn = (HttpURLConnection) connection;
-            ByteArrayOutputStream bout = new ByteArrayOutputStream();
-            String xmlInput
-                    = " <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:wsus=\"http://ws.movildata.com/ws/wsUsers\">\n"
-                    + " <soapenv:Header/>\n"
-                    + " <soapenv:Body>\n"
-                    + " <wsus:getLastLocationPlate>\n"
-                    + " <!--Optional:-->\n"
-                    + " <wsus:apikey>KO/TsiQRiqe0Pg+mGIeP+5RoG50sKv460pLZ9nKzVuJrUTfB6cUtqwNh5Tk7+Qxs</wsus:apikey>\n"
-                    + " <!--Optional:-->\n"
-                    + " <wsus:Plate>" + matriculaCabeza + "</wsus:Plate>\n"
-                    + " </wsus:getLastLocationPlate>\n"
-                    + " </soapenv:Body>\n"
-                    + " </soapenv:Envelope>";
-            byte[] buffer = new byte[xmlInput.length()];
-            buffer = xmlInput.getBytes();
-            bout.write(buffer);
-            byte[] b = bout.toByteArray();
-            String SOAPAction = "http://ws.movildata.com/ws/wsUsers/getLastLocationPlate";
-            // Set the appropriate HTTP parameters.
-            httpConn.setRequestProperty("Content-Length",
-                    String.valueOf(b.length));
-            httpConn.setRequestProperty("Content-Type", "text/xml; charset=utf-8");
-            httpConn.setRequestProperty("SOAPAction", SOAPAction);
-            httpConn.setRequestMethod("GET");
-            httpConn.setDoOutput(true);
-            httpConn.setDoInput(true);
-            OutputStream out = httpConn.getOutputStream();
-            //Write the content of the request to the outputstream of the HTTP Connection.
-            out.write(b);
-            out.close();
-            //Ready with sending the request.
-
-            //Read the response.
-            InputStreamReader isr
-                    = new InputStreamReader(httpConn.getInputStream(), StandardCharsets.UTF_8);
-            BufferedReader in = new BufferedReader(isr);
-
-            //Write the SOAP message response to a String.
-            while ((responseString = in.readLine()) != null) {
-                outputString = outputString + responseString;
-            }
-
-            SAXBuilder builder = new SAXBuilder();
-            Reader in2 = new StringReader(outputString);
-            Document doc;
-            Element root;
-            try {
-                doc = builder.build(in2);
-                root = doc.getRootElement();
-                Element body = root.getChild("Body", Namespace.getNamespace("S", "http://schemas.xmlsoap.org/soap/envelope/"));
-                Element getLastLocationPlateResponse = body.getChild("getLastLocationPlateResponse", Namespace.getNamespace("http://ws.movildata.com/ws/wsUsers"));
-                Element getLastLocationPlateResult = getLastLocationPlateResponse.getChild("getLastLocationPlateResult", Namespace.getNamespace("http://ws.movildata.com/ws/wsUsers"));
-                String line = getLastLocationPlateResult.getText();
-                if (line.contains("<loc>")) {
-                    String loc = line.substring(line.indexOf("<loc>") + "<loc>".length(), line.indexOf("</loc>"));
-                    int colonPosition = loc.indexOf(",");
-                    if (loc.length() > 3 && colonPosition != -1) {
-                        String latitud = loc.substring(0, colonPosition).replace('.', ',');
-                        String longitud = loc.substring(colonPosition + 1).replace('.', ',');
-                        posicion.add(latitud);
-                        posicion.add(longitud);
-                    }
-                }
-            } catch (JDOMException ex) {
-                LOGGER.log(Level.SEVERE, "JDOMException Matrícula: " + matriculaCabeza, ex.fillInStackTrace());
-            }
-        } catch (MalformedURLException ex) {
-            LOGGER.log(Level.SEVERE, "MalformedURLException Matrícula: " + matriculaCabeza, ex.fillInStackTrace());
-        } catch (IOException ex) {
-            LOGGER.log(Level.SEVERE, "IOException Matrícula: " + matriculaCabeza, ex.fillInStackTrace());
-        }
-        if (posicion.size() != 2) {
-            posicion.clear();
-            posicion.add("0");
-            posicion.add("0");
-        }
-        return posicion;
     }
 
     public static void sendTextFileToTiba() {
@@ -403,28 +304,10 @@ public class InterfazTIBA {
             for (String estadoId : imagenesNameFile.keySet()) {
                 for (String imagenName : imagenesNameFile.get(estadoId)) {
                     if (!imagenName.isEmpty()) {
-//                        File imageFile = new File(imagePath + imagenName);
-//
-//                        String remoteFile = imageFile.getName();
-//                        InputStream inputStream = new FileInputStream(imageFile);
-//
-//                        FileOutputStream outputStream2 = new FileOutputStream("./enviado_img/" + remoteFile);
-//                        OutputStream outputStream = ftpClient.storeFileStream(remoteFile);
-//                        byte[] bytesIn = new byte[4096];
-//                        int read = 0;
-//
-//                        while ((read = inputStream.read(bytesIn)) != -1) {
-//                            outputStream.write(bytesIn, 0, read);
-//                            outputStream2.write(bytesIn, 0, read);
-//                        }
-//                        inputStream.close();
-//                        outputStream.close();
-//                        outputStream2.close();
                         
                         String user = "EXOS";
                         String pass ="cbdriver@2021";
                         String sharedFolder="cbdriver/estados/";
-//                        String sharedFolder="cbdriver/medlog/";
                         String ipAddress = "192.168.0.9";
                         String imgPath = "smb://" + ipAddress + "/" +sharedFolder+"/" + imagenName;
                         NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication("",user, pass);
@@ -435,11 +318,11 @@ public class InterfazTIBA {
                         
                         // Opcional os2 (copia local)
                         FileOutputStream os2 = new FileOutputStream("./enviado_img/" + smbRemoteFileName);
-                        LOGGER.log(Level.INFO, null, "Obtenido el output stream local: " + smbRemoteFileName);
+                        //LOGGER.log(Level.INFO, null, "Obtenido el output stream local: " + smbRemoteFileName);
 
                         // Obligatorio os (copia al FTP)
                         OutputStream os = ftpClient.storeFileStream(smbRemoteFileName);
-                        LOGGER.log(Level.INFO, null, "Obtenido el output stream del dir FTP: " + imgPath);
+                        //LOGGER.log(Level.INFO, null, "Obtenido el output stream del dir FTP: " + imgPath);
                         
                         byte[] smbBytesIn = new byte[4096];
                         int smbRead = 0;
@@ -448,7 +331,7 @@ public class InterfazTIBA {
                             os.write(smbBytesIn, 0, smbRead);
                             os2.write(smbBytesIn, 0, smbRead);
                         }
-                        LOGGER.log(Level.INFO, null, "Imágenes guardadas en los directorios FTP y local.");
+                        LOGGER.log(Level.INFO, null, "Imagen "+imagenName+" guardada en los directorios FTP y local.");
                         smbfis.close();
                         os.close();
                         os2.close();
@@ -484,7 +367,7 @@ public class InterfazTIBA {
             for (String estadoId : imagenesNameFile.keySet()) {
                 pst.setString(1, estadoId);
                 pst.addBatch();
-                LOGGER.log(Level.INFO, null, "Enviado TIBA el estado: " + estadoId);
+                LOGGER.log(Level.INFO, null, "Marcado como Enviado TIBA el estado: " + estadoId);
             }
             pst.executeBatch();
         } catch (SQLException ex) {
@@ -513,14 +396,14 @@ public class InterfazTIBA {
                     + " ORDER BY h.f_planificada_llegada DESC LIMIT 1;";
             
             try (PreparedStatement ps = postgreCon.prepareStatement(selectSql)) {
-                Logger.getLogger(InterfazTIBA.class.getName()).log(Level.INFO, "Obteniendo posición de la matrícula " + matricula +  ".");
+                //Logger.getLogger(InterfazTIBA.class.getName()).log(Level.INFO, "Obteniendo posición de la matrícula " + matricula +  ".");
                 ps.setString(1, matricula);
                 ResultSet rs = ps.executeQuery();
                 while (rs.next()) {
                     posiciones.add(rs.getString(1));
                     posiciones.add(rs.getString(2));
                 }
-                Logger.getLogger(InterfazTIBA.class.getName()).log(Level.INFO, "Posición de la matrícula " + matricula + "obtenida: \n"
+                Logger.getLogger(InterfazTIBA.class.getName()).log(Level.INFO, "Posición matrícula " + matricula + ": \n"
                         + "latitud: " + posiciones.get(0) + ", longitud: " + posiciones.get(1));
             }
             
@@ -534,5 +417,67 @@ public class InterfazTIBA {
 
         }
         return posiciones;
+    }
+
+    /***
+     * Añade al fichero la posición actual de todos los vehículos que estén en hojas de rutas no finalizadas
+     * Solo se añaden vehiculos de los cuales no se hayan informado en el método anyadirEstadosPosicion
+     * para ello se utiliza la variable vehiculos enviados
+     */
+    private static void anyadirVehiculosConRutasNoFinalizadas() {
+        
+         try {
+            String postgresqlUrl = "jdbc:postgresql://" + postgresqlIp + ":" + postgresqlPort + "/"
+                    + postgresqlDatabaseName + "?user=" + postgresqlUser + "&password=" + postgresqlPassword;
+
+            Connection postgresqlConnection = DriverManager.getConnection(postgresqlUrl);
+
+            
+            String selectSql = "SELECT distinct r.codigo, r.expediente_ol, r.numero_contenedor1, r.numero_contenedor2, "
+                    + "v.matricula "
+                    + "FROM m_estado_ruta e INNER JOIN g_hoja_ruta r ON e.g_hoja_ruta_id = r.g_hoja_ruta_id "
+                    + "INNER JOIN m_cliente cli ON cli.m_cliente_id = r.m_cliente_id "
+                    + "INNER JOIN g_asignacion_hoja_ruta a ON a.g_hoja_ruta_id = r.g_hoja_ruta_id "
+                    + "INNER JOIN m_cabeza c ON c.m_cabeza_id = a.m_cabeza_id "
+                    + "INNER JOIN m_vehiculo v ON v.m_vehiculo_id = c.m_vehiculo_id "
+                    + "WHERE cli.codigo = '2200' AND e.tipo_aux IS NOT NULL  AND e.tipo_aux != ''  "
+                    + "and a.definitiva = true "
+                    + "AND (SELECT (MAX(e2.tipo_aux) IS NULL OR MAX(e2.tipo_aux) < '41') "
+                    + "FROM m_estado_ruta e2 WHERE e2.g_hoja_ruta_id = e.g_hoja_ruta_id AND e2.enviado_tiba = true) "
+                    //pongo esta condición porque había rutas anteriores a 2023 que no tenían estado 41 y se hubieran estado
+                    //enviando continuamente la posición de sus conductores.
+                    + "and r.f_planificada_llegada > '2023-01-01 00:00:00.000';";
+            try (Statement st = postgresqlConnection.createStatement(); ResultSet rs = st.executeQuery(selectSql)) {
+                ResultSetMetaData rsmd = rs.getMetaData();
+                while (rs.next()) {
+                    String codigo = rs.getString(1);
+                    String expedienteOL = rs.getString(2);
+                    String numeroCont1 = rs.getString(3);
+                    String numeroCont2 = rs.getString(4);
+                    String matricula = rs.getString(5);
+                    String tipoEvento = "1"; //Siempre uno segun nota  ~0022756 de Incidencia "0006945: Falla la integración con TIBA"
+
+                    if(!vehiculosEnviados.contains(matricula))
+                    {                   
+                        List<String> posicion = getPosicionBBDD(matricula);
+                        String fecha = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+                        String hora = LocalTime.now().format(DateTimeFormatter.ofPattern("HHmm"));
+
+                        BufferedWriter writer;
+                        try {
+                            writer = new BufferedWriter(new FileWriter(textFile, true));
+                            writer.append(' ');
+                            writer.append("\"" + codigo + "\";\"" + expedienteOL + "\";\"" + numeroCont1 + "\";\"" + numeroCont2 + "\";" + tipoEvento + ";\"" + matricula + "\";"
+                                    + posicion.get(0) + ";" + posicion.get(1) + ";" + fecha + ";" + hora + ";\"\"\n");
+                            writer.close();
+                        } catch (IOException ex) {
+                            Logger.getLogger(InterfazTIBA.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InterfazTIBA.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
